71ee257ba749219483f2a0b13ecf78c152e4b02a
sohaib88 <roomi9966@gmail.com>
2022-10-26 16:37:23 +0500

    Add more Data

---
+++ src/Pagination.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error('something went wrong while requesting posts');
+      })
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) return <h1>{error}</h1>;
+
+  return <div></div>;
+}
